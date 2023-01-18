9d4184742575e41a216ce7fdfb7af2f3bfb286bb
kamrul373 <textile.zero@gmail.com>
2023-01-18 07:56:36 +0600

    isArray check problem added

---
+++ isArray.js
@@ -0,0 +1,7 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const person = {name : "Alice", age : 17}
+let result =  Array.isArray(fruits);
+let r = Array.isArray(person)
+
+console.log(result)
+console.log(r)
